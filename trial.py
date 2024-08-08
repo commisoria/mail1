ce7574651239c2eee4dcbd8d7955f4220383ec7c
commisoria <115154782+commisoria@users.noreply.github.com>
2024-08-08 17:28:28 +0300

    Update trial.py

--- trial.py
+++ trial.py
@@ -1,14 +1,20 @@
 import yfinance as yf
 import streamlit as st
 from time import sleep
-st.selectbox(label, options, index=0, format_func=special_internal_function, key=None, help=None, on_change=None, args=None, kwargs=None, *, placeholder="Choose an option", disabled=False, label_visibility="visible")
+options = ['GC=F', 'BTC-USD', 'PAXG-USD', 'BNB-USD', 'TRY=X']
 
-def gold():
-    gold = yf.download(tickers="GC=F", period="1d", interval="15m")
-    latest_price = gold['Close'].iloc[-1]
-    return latest_price
+# Create the select box
+selected_option = st.selectbox('Select an option', options)
+button=st.button('Calculate')
 
-while True:
-    gold=gold()
-    st.write(gold)
-    sleep(120)
+if button:
+    def asset(selected_option):
+        gold = yf.download(tickers=selected_option, period="1d", interval="15m")
+        latest_price = gold['Close'].iloc[-1]
+        return latest_price
+    asset=asset()
+    st.write(asset)
+    
+    
+
+
