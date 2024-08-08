19ed5892d3770d24ae3eb47fa7b85f594ae31e18
commisoria <115154782+commisoria@users.noreply.github.com>
2024-08-08 17:05:58 +0300

    Update trial.py

--- trial.py
+++ trial.py
@@ -1,30 +1,11 @@
-import smtplib
 import yfinance as yf
+import streamlit as st
 
-gold = yf.download(tickers="GC=F", period="1d", interval="15m")
-latest_price = gold['Close'].iloc[-1]
-
-
-def prompt(prompt):
-    return input(prompt).strip()
+def gold():
+    gold = yf.download(tickers="GC=F", period="1d", interval="15m")
+    latest_price = gold['Close'].iloc[-1]
 
 
-fromaddr = prompt("From:lex.commisoria@gmail.com")
-toaddrs = prompt("To:ism.bolat@hotmail.com").split()
-# Add the From: and To: headers at the start!
-msg = (f"From: %s\r\nTo: %s\r\n\r\n {latest_price}"
-       % (fromaddr, ", ".join(toaddrs)))
-while True:
-    try:
-        line = input()
-    except EOFError:
-        break
-    if not line:
-        break
-    msg = msg + line
+gold=gold()
 
-print("Message length is", len(msg))
-server = smtplib.SMTP('localhost', 1025)
-server.set_debuglevel(1)
-server.sendmail(fromaddr, toaddrs, msg)
-server.quit()
+st.write(gold)
