694216cd95f83da2db406d97dd7dd061ce77220d
commisoria <115154782+commisoria@users.noreply.github.com>
2024-08-08 17:42:18 +0300

    Update trial.py

--- trial.py
+++ trial.py
@@ -7,14 +7,16 @@ options = ['GC=F', 'BTC-USD', 'PAXG-USD', 'BNB-USD', 'TRY=X']
 selected_option = st.selectbox('Select an option', options)
 button=st.button('Calculate')
 
+def asset(selected_option):
+    gold = yf.download(tickers=selected_option, period="1d", interval="15m")
+    latest_price = gold['Close'].iloc[-1]
+    return latest_price
+
+asset=asset(selected_option)
 while True:
     if button:
-        def asset(selected_option):
-            gold = yf.download(tickers=selected_option, period="1d", interval="15m")
-            latest_price = gold['Close'].iloc[-1]
-            return latest_price
-         asset=asset(selected_option)
         st.write(asset)
+       
 
     
     
