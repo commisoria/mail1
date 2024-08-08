3128a716001a7126dd174870b227b84e005267a0
commisoria <115154782+commisoria@users.noreply.github.com>
2024-08-08 17:09:55 +0300

    Update trial.py

--- trial.py
+++ trial.py
@@ -1,12 +1,13 @@
 import yfinance as yf
 import streamlit as st
+from time import sleep
 
 def gold():
     gold = yf.download(tickers="GC=F", period="1d", interval="15m")
     latest_price = gold['Close'].iloc[-1]
     return latest_price
 
-
-gold=gold()
-
-st.write(gold)
+while True:
+    gold=gold()
+    st.write(gold)
+    sleep(120)
