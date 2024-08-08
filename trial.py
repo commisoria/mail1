c2de4fc05de8a8bdebf287fbfe7b05f83bd81a7e
commisoria <115154782+commisoria@users.noreply.github.com>
2024-08-08 17:14:52 +0300

    Update trial.py

--- trial.py
+++ trial.py
@@ -1,6 +1,7 @@
 import yfinance as yf
 import streamlit as st
 from time import sleep
+st.selectbox(label, options, index=0, format_func=special_internal_function, key=None, help=None, on_change=None, args=None, kwargs=None, *, placeholder="Choose an option", disabled=False, label_visibility="visible")
 
 def gold():
     gold = yf.download(tickers="GC=F", period="1d", interval="15m")
