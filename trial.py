64a1520242c0e535233838f0f855121743d3ec77
commisoria <115154782+commisoria@users.noreply.github.com>
2024-08-08 17:08:40 +0300

    Update trial.py

--- trial.py
+++ trial.py
@@ -4,6 +4,7 @@ import streamlit as st
 def gold():
     gold = yf.download(tickers="GC=F", period="1d", interval="15m")
     latest_price = gold['Close'].iloc[-1]
+    return latest_price
 
 
 gold=gold()
